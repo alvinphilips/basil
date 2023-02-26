22197ba89808d70a3be52f5b8757323e0ee9f39c
Alvin Philips <alvinphilips257@gmail.com>
2023-02-26 17:27:13 +0530

    Fixed map and added country highlighting

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-slate-900 text-white py-2 flex flex-col justify-center items-center">
+      <span>
+        Copyright &copy; 2023 Alvin Philips. You are now legally obligated to
+        eat noodles :P
+      </span>
+      <span>Also, thanks for scrolling :&#41;</span>
+    </footer>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 import { MdOutlineRamenDining } from "react-icons/md";
 import { TbSwords } from "react-icons/tb";
 import { GiBread } from "react-icons/gi";
 import { IoBeerOutline } from "react-icons/io5";
 import { AiFillCar } from "react-icons/ai";
 import { FaLeaf } from "react-icons/fa";
-import React from "react";
+import React, { useState } from "react";
 import {
   ComposableMap,
   Geographies,
@@ -16,19 +16,41 @@ import {
 } from "react-simple-maps";
 
 const geoUrl =
-  "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries-sans-antarctica.json";
+  "https://raw.githubusercontent.com/gotham13/World-Map-India-Complete/master/TopoJson/topo.json";
 
-function MapChart() {
+type Area = string;
+type CoolnessCategory = "noodles" | "bread" | "beer" | "swords" | "cars";
+
+const noodle_data = new Array<[string, string]>(
+  ["geo-7", "maggi"],
+  ["geo-239", "ramen"],
+  ["geo-4", "lo mein"]
+);
+const noodle_map = new Map<Area, string>(noodle_data);
+
+type MapChartProps = {
+  content: string;
+  setContentOnClick: React.Dispatch<React.SetStateAction<string>>;
+  category: CoolnessCategory;
+};
+
+function MapChart({ content, setContentOnClick, category }: MapChartProps) {
   return (
     <ComposableMap projection="geoMercator" className="w-full h-full">
       <ZoomableGroup center={[105, 20]} zoom={3.5}>
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
             geographies.map((geo) => (
+              /** ts-ignore */
               <Geography
                 key={geo.rsmKey}
                 geography={geo}
-                className="fill-amber-100 stroke-slate-700"
+                className={`${
+                  geo.rsmKey == content
+                    ? "fill-sky-400 hover:fill-sky-200"
+                    : "fill-amber-100  hover:fill-amber-300"
+                } stroke-stone-700 stroke-[0.5]`}
+                onClick={() => setContentOnClick(geo.rsmKey)}
               />
             ))
           }
@@ -38,9 +60,9 @@ function MapChart() {
   );
 }
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
+  let [country, setCountry] = useState("geo-7");
+  let [category, setCategory] = useState<CoolnessCategory>("noodles");
   return (
     <>
       <Head>
@@ -52,36 +74,51 @@ export default function Home() {
       <header className="w-full h-16 bg-slate-900 flex justify-center items-center text-white text-2xl font-semibold font-serif">
         <FaLeaf />
         &nbsp;
-        <h1>basil</h1>
+        <h1>basil | {country}</h1>
       </header>
       <main className="w-full sm:flex h-screen">
-        <div className=" bg-gradient-to-b from-purple-500 to-violet-700 h-full w-full sm:max-w-sm flex justify-center items-start pt-10 px-8">
+        <div className="bg-gradient-to-b from-purple-500 to-violet-700 h-full flex w-full sm:max-w-sm  justify-center items-start pt-10 px-8">
           <ul className="grid sm:grid-cols-4 gap-4 w-full text-violet-100">
-            <li className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center">
+            <li
+              className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center"
+              onClick={() => setCategory("noodles")}
+            >
               <MdOutlineRamenDining className="text-4xl" />
               <span className="text-lg sm:text-xs ml-5 sm:ml-0 sm:text-violet-200">
                 noodles
               </span>
             </li>
-            <li className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center">
+            <li
+              className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center"
+              onClick={() => setCategory("bread")}
+            >
               <GiBread className="text-4xl" />
               <span className="text-lg sm:text-xs ml-5 sm:ml-0 sm:text-violet-200">
                 bread
               </span>
             </li>
-            <li className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center">
+            <li
+              className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center"
+              onClick={() => setCategory("swords")}
+            >
               <TbSwords className="text-4xl" />
               <span className="text-lg sm:text-xs ml-5 sm:ml-0 sm:text-violet-200">
                 swords
               </span>
             </li>
-            <li className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center">
+            <li
+              className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center"
+              onClick={() => setCategory("beer")}
+            >
               <IoBeerOutline className="text-4xl" />
               <span className="text-lg sm:text-xs ml-5 sm:ml-0 sm:text-violet-200">
                 beer
               </span>
             </li>
-            <li className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center">
+            <li
+              className="w-full sm:w-16 h-20 bg-violet-900 rounded-lg flex px-5 sm:flex-col sm:justify-center items-center"
+              onClick={() => setCategory("cars")}
+            >
               <AiFillCar className="text-4xl" />
               <span className="text-lg sm:text-xs ml-5 sm:ml-0 sm:text-violet-200">
                 cars
@@ -90,16 +127,14 @@ export default function Home() {
           </ul>
         </div>
         <div className="bg-sky-900 h-full w-full">
-          <MapChart />
+          <MapChart
+            content={country}
+            setContentOnClick={setCountry}
+            category={category}
+          />
         </div>
       </main>
-      <footer className="bg-slate-900 text-white py-2 flex flex-col justify-center items-center">
-        <span>
-          Copyright &copy; 2023 Alvin Philips. You are now legally obligated to
-          eat noodles :P
-        </span>
-        <span>Also, thanks for scrolling :&#41;</span>
-      </footer>
+      <Footer />
     </>
   );
 }
